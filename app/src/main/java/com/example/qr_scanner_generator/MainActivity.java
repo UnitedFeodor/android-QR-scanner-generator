457a7a1a5791c8aff8a313d96b4293c22bba32e8
feodor <fedor15zil@gmail.com>
2023-03-03 18:58:58 +0300

    file path and permission fixed

--- app/src/main/java/com/example/qr_scanner_generator/MainActivity.java
+++ app/src/main/java/com/example/qr_scanner_generator/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.qr_scanner_generator;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -16,6 +22,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
@@ -36,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{READ_EXTERNAL_STORAGE},1);
 
 
         // initializing all variables.
@@ -111,6 +122,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted and now can proceed
+                    Toast.makeText(MainActivity.this, "Permission granted!!!", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage :(", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // add other cases for more permissions
+        }
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Fix no activity available
         super.onActivityResult(requestCode, resultCode, data);
@@ -120,7 +154,10 @@ public class MainActivity extends AppCompatActivity {
             case PICKFILE_RESULT_CODE:
                 if (resultCode == RESULT_OK) {
                     Toast.makeText(MainActivity.this, "Trying to decode the QR...", Toast.LENGTH_SHORT).show();
-                    String filePath = data.getData().getPath();
+                    //String filePath = data.getData().getPath();
+                    String filePath = Utils.getActualPath(this, data.getData());
+
+
                     if(filePath.contains("document/raw:")){
                         filePath = filePath.replace("/document/raw:","");
                     }
